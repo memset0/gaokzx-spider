a258bb3f31401a31269e0a1cd22ceedad48d816d
memset0 <memset0@outlook.com>
2020-07-13 04:06:13 +0800

    v5

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const rd = require('rd');
+const pdf = require('html-pdf');
 const path = require('path');
 const images = require('images');
 
@@ -21,10 +22,11 @@ function listAll(dir) {
 }
 
 function buildHtml(dir) {
+	let title = path.basename(dir);
 	let files = rd.readFileSync(dir);
 	console.log(dir, files);
 
-	let html = `<title>${path.basename(dir)}</title><style>body{margin: 0;}</style>`;
+	let html = `<title>${title}</title><style>body{margin: 0;}</style>`;
 	let img = src => (`<img src="${src}" style="width: 80%; display: block; margin: auto;"/>`);
 	let divider = '<div style="width: 0; height: 0; page-break-after: always;"></div>';
 
@@ -43,9 +45,23 @@ function buildHtml(dir) {
 			html += divider;
 		}
 		sum += height / width;
-		html += img(path.relative(dir, file));
+		html += img('file://' + file);
 	})
-	fs.writeFileSync(path.join(dir, 'index.html'), html);
+	fs.writeFileSync(path.join(dir, title + '.html'), html);
+	pdf.create(html, {
+		border: {
+			top: '10mm',
+			bottom: '5mm',
+			right: '10mm',
+			left: '10mm',
+		},
+		header: {
+			height: '5mm',
+			contents: `<div style="font-size: 10px; text-align: center">${title}（{{page}} / {{pages}}）@memset0.</div>`
+		}
+	}).toFile(path.join(dir, title + '.pdf'), function (err, res) {
+		console.log(res.filename);
+	});
 }
 
 listAll(path.join(__dirname, 'dist')).forEach(dir => {
