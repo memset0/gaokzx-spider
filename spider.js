7721eb6bac6f281eeb5690b0718d712d75773fff
memset0 <memset0@outlook.com>
2020-07-13 02:28:29 +0800

    v1.1

--- spider.js
+++ spider.js
@@ -3,34 +3,33 @@ const url = require('url');
 const path = require('path');
 const YAML = require('yaml');
 const cheerio = require('cheerio');
+const download = require('download');
 const superagent = require('superagent');
 
 async function downloadFile(src, dst) {
-	console.log('download', src, dst);
-	return await superagent
-		.get(src)
-		.then(function (res) {
-			fs.writeFileSync(dst, res.body, 'binary');
-		});
+	fs.mkdirSync(path.dirname(dst), { recursive: true });
+	fs.writeFileSync(dst, await download(src));
 }
 
 async function spiderPage(pageCurrent) {
 	return await superagent
 		.get(pageCurrent)
+		.set('user-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36')
 		.then(function (res) {
-			let $ = cheerio.load(res.body);
+			let $ = cheerio.load(res.text);
 
 			let title = $('.page-content h1').text().trim();
-			let image = $('#content .center img').attr('src');
+			let image = $('#content img').attr('src');
+			console.log(title, image);
 
 			downloadFile(
 				url.resolve(pageCurrent, image),
-				path.join(__dirname, 'dist', title, path.basename(url, path.extname(url)) + path.extname(image))
+				path.join(__dirname, 'dist', title, path.basename(pageCurrent, path.extname(pageCurrent)) + path.extname(image))
 			);
 
-			let pageNext = $('a.next').href();
+			let pageNext = $('a.next').attr('href');
 			if (pageNext !== '' && pageNext !== '#') {
-				spiderPage(pageNext);
+				spiderPage(url.resolve(pageCurrent, pageNext));
 			}
 		});
 }
